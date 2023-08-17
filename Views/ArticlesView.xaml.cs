936bd9ad6c84afbf29d9fca58fbd7213d594b394
Mislav Matijević <mmatijevi@foi.hr>
2023-08-17 10:50:13 +0200

    Add a view for displaying articles.

---
+++ Views/ArticlesView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ScientificArticleManagerWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for ArticlesView.xaml
+    /// </summary>
+    public partial class ArticlesView : UserControl
+    {
+        public ArticlesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
