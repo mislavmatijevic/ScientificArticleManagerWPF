4ab5dde6877c03af75c804d7be7de3c913d61231
Mislav Matijević <mmatijevi@foi.hr>
2023-08-17 15:40:08 +0200

    Introduce window for editing of the articles.

---
+++ Converters/PublicationDateEditConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ScientificArticleManagerWPF.Converters
+{
+    internal class PublicationDateEditConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime date = (DateTime)value;
+            return date.ToString("dd. MM. yyyy.");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime date = DateTime.Parse((string)value);
+            return date;
+        }
+    }
+}
--- ViewModel/ArticleViewModel.cs
+++ ViewModel/ArticleViewModel.cs
@@ -1,16 +1,37 @@
 ﻿using ScientificArticleManagerWPF.Model;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace ScientificArticleManagerWPF.ViewModel
 {
-    internal class ArticleViewModel
+    internal class ArticleViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<ScientificArticle> Articles { get; set; }
+        private ScientificArticle? _selectedArticle;
+
+        public ScientificArticle? SelectedArticle
+        {
+            get => _selectedArticle;
+            set
+            {
+                _selectedArticle = value;
+                OnPropertyChanged(nameof(SelectedArticle));
+            }
+        }
 
         public ArticleViewModel()
         {
             var articles = MockScientificArticleRepository.Instance.GetArticles();
             Articles = new ObservableCollection<ScientificArticle>(articles);
+
+            _selectedArticle = Articles[2];
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
