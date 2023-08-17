f710e8bef9164ab2225bb86e56c27d680831fbae
Mislav Matijević <mmatijevi@foi.hr>
2023-08-17 14:18:15 +0200

    Improved visual representation of articles.

---
+++ Converters/PublicationDateConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ScientificArticleManagerWPF.Converters
+{
+    internal class PublicationDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime dateTime = (DateTime)value;
+            return dateTime.ToString("MMMM yyyy.", new CultureInfo("en-US"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
