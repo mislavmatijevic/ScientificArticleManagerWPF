d66d85682989ecc29d321f1e12434b06082fe2e7
Mislav Matijević <mmatijevi@foi.hr>
2023-08-17 10:42:23 +0200

    Add simple ViewModel for articles.

---
+++ ViewModel/ArticleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ScientificArticleManagerWPF.Model;
+using System.Collections.ObjectModel;
+
+namespace ScientificArticleManagerWPF.ViewModel
+{
+    internal class ArticleViewModel
+    {
+        public ObservableCollection<ScientificArticle> Articles { get; set; }
+
+        public ArticleViewModel()
+        {
+            var articles = MockScientificArticleRepository.Instance.GetArticles();
+            Articles = new ObservableCollection<ScientificArticle>(articles);
+        }
+    }
+}
