88d5e0905e1b765441fe092a1f6d5a785598c9ca
Mislav Matijević <mmatijevi@foi.hr>
2023-08-17 09:51:17 +0200

    Add models for describing an article.

---
+++ Model/ScientificArticle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScientificArticleManagerWPF.Model
+{
+    internal class ScientificArticle
+    {
+        public ScientificArticle(string title, List<ScientificArticleAuthor> authors, string articleAbstract, DateTime publicationDate, List<string> keywords, string doi, string citation)
+        {
+            Title = title;
+            Authors = authors;
+            Abstract = articleAbstract;
+            PublicationDate = publicationDate;
+            Keywords = keywords;
+            Doi = doi;
+            Citation = citation;
+        }
+
+        public string Title { get; set; }
+        public List<ScientificArticleAuthor> Authors { get; set; }
+        public string Abstract { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public List<string> Keywords { get; set; }
+        public string Doi { get; set; }
+        public string Citation { get; set; }
+    }
+}
---
+++ Model/ScientificArticleAuthor.cs
@@ -0,0 +1,18 @@
+﻿namespace ScientificArticleManagerWPF.Model
+{
+    public class ScientificArticleAuthor
+    {
+        public ScientificArticleAuthor(string firstName, string lastName, string affiliation, string academicDegree)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Affiliation = affiliation;
+            AcademicDegree = academicDegree;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Affiliation { get; set; }
+        public string AcademicDegree { get; set; }
+    }
+}
